ea115960070f3fcd465f15174c4655bf99652525
miguel tostado <mtalamo2000@gmail.com>
2025-06-02 19:19:43 -0600

    First attempt in TwoSum

---
+++ TwoSum/TwoSum.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+
+
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let x = 0; x < nums.length; x++) {
+      if (i != x) {
+        if (nums[i] + nums[x] == target) {
+          return [i, x];
+        }
+      }
+    }
+  }
+  return;
+};
+
+let x = [
+  [[2,7,11,15], 9],
+  [[3,2,4], 6],
+  [[3,3], 6]
+]
+
+x.forEach(element => {
+  console.log(twoSum(element[0], element[1]));
+});
+
