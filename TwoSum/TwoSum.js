a9f621a575591257d2b44fb8b12e78b11d084d5e
miguel tostado <mtalamo2000@gmail.com>
2025-06-02 19:27:37 -0600

    Minor change in TwoSum

--- TwoSum/TwoSum.js
+++ TwoSum/TwoSum.js
@@ -4,15 +4,11 @@
  * @return {number[]}
  */
 
-
-
 var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let x = 0; x < nums.length; x++) {
-      if (i != x) {
-        if (nums[i] + nums[x] == target) {
-          return [i, x];
-        }
+      if (i != x && nums[i] + nums[x] == target) {
+        return [i, x];
       }
     }
   }
@@ -22,7 +18,8 @@ var twoSum = function (nums, target) {
 let x = [
   [[2,7,11,15], 9],
   [[3,2,4], 6],
-  [[3,3], 6]
+  [[3,3], 6],
+  [[-1, -4, -5, 8], 3]
 ]
 
 x.forEach(element => {
