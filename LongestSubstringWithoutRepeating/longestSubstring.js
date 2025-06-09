125d7fb66218dc4c3a75051366fd62e05efb64a0
miguel tostado <mtalamo2000@gmail.com>
2025-06-09 13:16:26 -0600

    Longest substring, second attempt using recursive

--- LongestSubstringWithoutRepeating/longestSubstring.js
+++ LongestSubstringWithoutRepeating/longestSubstring.js
@@ -4,27 +4,62 @@
  */
 var lengthOfLongestSubstring = function(s) {
   if (s.split("").length == 0) { return 0; }
-  let last_letters = [];
-  let largest_solution = 0;
-
-  let test = [];
-
-
-  s.split("").forEach(e => {
-    if (last_letters.includes(e)) {
-      largest_solution = last_letters.length > largest_solution ? last_letters.length : largest_solution;
-      test.push(...last_letters);
-      last_letters = [];
+  let longestFound = 0;
+  let current_length = 0;
+  console.log(s.split(""));
+  for (let i = 0; i < s.split("").length; i++) {
+    current_length = recursiveFunction(s.split(""), [], i);
+    if (longestFound < current_length) {
+      longestFound = current_length;
     }
-    last_letters.push(e);
-    largest_solution = last_letters.length > largest_solution ? last_letters.length : largest_solution
-  });
-  return largest_solution;
+  }
+  return longestFound;
 };
+
+
+const recursiveFunction = function(arr, current_arr, n) {
+  let longestSubstring = 0;
+  if (!current_arr.includes(arr[n])) {
+    current_arr.push(arr[n]);
+    if (n < (arr.length - 1)) {
+      longestSubstring = recursiveFunction(arr, current_arr, n+1);
+    } else {
+      return current_arr.length;
+    }
+  } else {
+    return current_arr.length;
+  }
+  return longestSubstring;
+}
 
 
 
 const s = "dvdf";
+const a = "abcabcbb";
 
 
-console.log(lengthOfLongestSubstring(s));
+lengthOfLongestSubstring(a);
+
+
+
+
+// first attempt
+// var lengthOfLongestSubstring = function(s) {
+//   if (s.split("").length == 0) { return 0; }
+//   let last_letters = [];
+//   let largest_solution = 0;
+
+//   let test = [];
+
+
+//   s.split("").forEach(e => {
+//     if (last_letters.includes(e)) {
+//       largest_solution = last_letters.length > largest_solution ? last_letters.length : largest_solution;
+//       test.push(...last_letters);
+//       last_letters = [];
+//     }
+//     last_letters.push(e);
+//     largest_solution = last_letters.length > largest_solution ? last_letters.length : largest_solution
+//   });
+//   return largest_solution;
+// };
