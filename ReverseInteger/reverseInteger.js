f1f16a80b02a3061334748629d059ee0f229d2bb
miguel tostado <mtalamo2000@gmail.com>
2025-06-21 14:41:57 -0600

    Reverse Integer CHANGE, included validation for the 32-bit limit

--- ReverseInteger/reverseInteger.js
+++ ReverseInteger/reverseInteger.js
@@ -13,6 +13,12 @@ var reverse = function(x) {
     temp = Math.floor(temp / 10);
   }
   reverse_number = Number(temp_array.join(''));
+  // THIS PART OF THE CODE IS TO CHECK FOR THE 32-BIT LIMIT
+  // IN JAVASCRIPT IS NOT NECESSARY, BUT THE PROBLEM ASK US TO CONSIDER IT.
+  if (reverse_number > 2147483647 || reverse_number < -2147483648) {
+    return 0;
+  }
+  
   if (isNegative) {
     reverse_number *= -1;
   }
@@ -20,7 +26,14 @@ var reverse = function(x) {
 
 };
 
-const x = -1234;
-let result;
-result = reverse(x);
-console.log(result);
+const x = [
+  123,
+  -123,
+  8765430,
+  -76550340,
+  1534236469
+];
+
+x.forEach(e => {
+  console.log(reverse(e));
+});
