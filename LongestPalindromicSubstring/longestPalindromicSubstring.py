cb3564da745f2a2502cbb42591b93f81f2fbaed3
miguel tostado <mtalamo2000@gmail.com>
2025-07-29 16:47:07 -0600

    Longest palindromic substring python

---
+++ LongestPalindromicSubstring/longestPalindromicSubstring.py
@@ -0,0 +1,33 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longestLength = 0
+        palindrome = ""
+
+        left2 = 0
+        right2 = 0
+
+        for index in range(len(s)):
+            left, right = self.isPalindrome(s, index, index)
+            if (right - left + 1 > longestLength):
+                longestLength = right - left + 1
+                palindrome = s[left:right + 1]
+            
+            left2, right2 = self.isPalindrome(s, index, index + 1)
+            if (right2 - left2 + 1 > longestLength):
+                longestLength = right2 - left2 + 1
+                palindrome = s[left2:right2 + 1]
+
+        return palindrome
+    
+    def isPalindrome(self, s, left, right):
+        while left >= 0 and right <= len(s) - 1:
+            if (s[left] != s[right]):
+                break
+            left -= 1
+            right += 1
+        return left + 1, right - 1
+    
+
+s = "cddf"
+sol = Solution()
+print(sol.longestPalindrome(s))
