686c7d8db769ccf13e61a506fe2509ed3a9d78cb
miguel tostado <mtalamo2000@gmail.com>
2025-07-30 17:21:00 -0600

    nQueens Updated visual solution

--- N-Queens/nQueens.py
+++ N-Queens/nQueens.py
@@ -1,46 +1,40 @@
 class Solution:
-  def solveNQueens(self, n):
-    currentPath = []
-    solutionArr = []
-    self.exploreCells(n, 0, currentPath, solutionArr)
-    solutionVisual = []
-    for solutions in solutionArr:
-       self.generateVisualSolution(n, solutions, solutionVisual)
-    return solutionVisual
+    def solveNQueens(self, n):
+        currentPath = []
+        solutionArr = []
+        visualArr = []
+        self.exploreCells(n, 0, currentPath, solutionArr, visualArr)
+        return solutionArr
   
-  def generateVisualSolution(self, n, arr, solutionVisual):
-    lineArr = []
-    for i in arr:
-        lineStr = ""
-        for j in range(n):
-            if (j == i[0]):
-                lineStr += "Q"
-            else:
-                lineStr += "."
-        lineArr.append(lineStr)
-    solutionVisual.append([*lineArr])
-    return
-  
-  def exploreCells(self, n, row, currentPath, arrResult):
+    def exploreCells(self, n, row, currentPath, arrResult, visualArr):
     
-    if row > n - 1:
-       arrResult.append([*currentPath])
-       return
+        if row > n - 1:
+            arrResult.append([*visualArr])
+            return
     
-    for col in range(n):
-        if self.validCell([col, row], currentPath):
-           currentPath.append([col, row])
-           self.exploreCells(n, row + 1, currentPath, arrResult)
-           currentPath.pop()
-    return
+        strLine = "." * n
+        for col in range(n):
+            if self.validCell([col, row], currentPath):
+                currentPath.append([col, row])
+                visualArr.append(strLine[:col] + "Q" + strLine[col + 1:])
+                self.exploreCells(n, row + 1, currentPath, arrResult, visualArr)
+                currentPath.pop()
+                visualArr.pop()
+        return
   
-  def validCell(self, cell, arr):  
-     for items in arr:
-        if items[0] == cell[0] or items[0] + items[1] == cell[0] + cell[1] or items[0] - items[1] == cell[0] - cell[1]:
-           return False
-     return True
+    def validCell(self, cell, arr):  
+        for items in arr:
+            if items[0] == cell[0] or items[0] + items[1] == cell[0] + cell[1] or items[0] - items[1] == cell[0] - cell[1]:
+                return False
+        return True
 
-n = 4
+n = 5
 sol = Solution()
-sol.solveNQueens(n)
 
+response = sol.solveNQueens(n)
+print(response)
+
+for res in response:
+    for i in res:
+        print(i)
+    print("-----------------------------------")
