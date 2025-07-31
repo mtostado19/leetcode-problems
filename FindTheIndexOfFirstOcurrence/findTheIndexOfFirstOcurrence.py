1a47834ab64f867948061d3c1b8626d3b9727abf
miguel tostado <mtalamo2000@gmail.com>
2025-07-30 21:55:51 -0600

    Find needle in haystack, manual solution added

--- FindTheIndexOfFirstOcurrence/findTheIndexOfFirstOcurrence.py
+++ FindTheIndexOfFirstOcurrence/findTheIndexOfFirstOcurrence.py
@@ -2,8 +2,28 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         return haystack.find(needle)
     
-haystack = "qwewasdty"
+haystack = "abwswasdpowiu"
 needle = "wasd"
 
 sol = Solution()
-sol.strStr(haystack, needle)
+print(sol.strStr(haystack, needle))
+
+# Manual solution - Less efficient
+# def manualSolution():
+#     index = 0
+#     answer = -1
+#     for i in range(len(haystack)):
+
+#         if haystack[i] == needle[index]:
+#             index += 1
+#             if index == len(needle):
+#                 answer = i - (len(needle) - 1)
+#                 break
+#         else:
+#             if haystack[i] == needle[0]:
+#                 index = 1
+#             else: 
+#                 index = 0
+#     return answer
+
+# print(manualSolution())
